493fefd6863337df2d90a35298cdf41461a48243
katyabarkalova <guliaev.p2014@yandex.ru>
2023-07-10 21:42:48 +0300

    1
    1xd

---
+++ src/main/java/com/example/testrestfullcsv/TestResTfullCsvApplication.java
@@ -0,0 +1,13 @@
+package com.example.testrestfullcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestResTfullCsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestResTfullCsvApplication.class, args);
+    }
+
+}
